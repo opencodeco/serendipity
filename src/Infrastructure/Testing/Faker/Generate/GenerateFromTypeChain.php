b70fc6a1819b252c9d79e88aa4baba7c4b42d880
William Correa <wilcorrea@gmail.com>
2025-02-27 03:50:01 -0300

    feat: bootstrap

--- src/Infrastructure/Testing/Faker/Generate/GenerateFromTypeChain.php
+++ src/Infrastructure/Testing/Faker/Generate/GenerateFromTypeChain.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Serendipity\Infrastructure\Testing\Faker\Generate;
 
 use DateTime;
+use DateTimeImmutable;
+use ReflectionException;
 use ReflectionParameter;
 use Serendipity\Domain\Support\Value;
 use Serendipity\Domain\Support\Values;
@@ -12,6 +14,9 @@ use Throwable;
 
 final class GenerateFromTypeChain extends Chain
 {
+    /**
+     * @throws ReflectionException
+     */
     public function resolve(ReflectionParameter $parameter, ?Values $preset = null): ?Value
     {
         $type = $this->extractType($parameter);
@@ -22,13 +27,16 @@ final class GenerateFromTypeChain extends Chain
             return new Value($this->engine->format($type));
         } catch (Throwable) {
         }
+        if ($type === 'object' && class_exists($type)) {
+            return new Value($this->fake($type, $preset?->toArray() ?? []));
+        }
         return match ($type) {
             'int' => new Value($this->engine->numberBetween(1, 100)),
             'string' => new Value($this->engine->word()),
             'bool' => new Value($this->engine->boolean()),
             'float' => new Value($this->engine->randomFloat(2, 1, 100)),
             'array' => new Value($this->engine->words()),
-            \DateTimeImmutable::class, DateTime::class => new Value($this->engine->dateTime()),
+            DateTimeImmutable::class, DateTime::class => new Value($this->engine->dateTime()),
             default => parent::resolve($parameter, $preset),
         };
     }
