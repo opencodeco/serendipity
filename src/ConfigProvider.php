b9acdb17039371fc8b232cef6ff1318c0f5a15f5
William Correa <wilcorrea@gmail.com>
2025-02-28 14:01:47 -0300

    feat: allow configure GCP logging

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -4,13 +4,6 @@ declare(strict_types=1);
 
 namespace Serendipity;
 
-use Psr\Container\ContainerInterface;
-use Psr\Log\LoggerInterface;
-use Serendipity\Infrastructure\Logging\EnvironmentLoggerFactory;
-
-use function Hyperf\Support\env;
-use function Serendipity\Type\Cast\toString;
-
 class ConfigProvider
 {
     /**
@@ -19,11 +12,7 @@ class ConfigProvider
     public function __invoke(): array
     {
         return [
-            'dependencies' => [
-                LoggerInterface::class => static fn (ContainerInterface $container) => $container
-                    ->get(EnvironmentLoggerFactory::class)
-                    ->make(toString(env('APP_ENV', 'dev'))),
-            ],
+            'dependencies' => [],
             'annotations' => [
                 'scan' => [
                     'paths' => [
