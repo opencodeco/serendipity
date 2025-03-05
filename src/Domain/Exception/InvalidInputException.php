d3e258eeeac85d8d66b4f13db068e3efc9f5cea2
William Correa <wilcorrea@gmail.com>
2025-03-04 21:02:46 -0300

    feat: new Invalid Input exception

---
+++ src/Domain/Exception/InvalidInputException.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Serendipity\Domain\Exception;
+
+use Exception;
+use Throwable;
+
+class InvalidInputException extends Exception
+{
+    /**
+     * @param array<string,string> $errors
+     * @param Throwable|null $previous
+     * @param int $code
+     */
+    public function __construct(
+        private readonly array $errors,
+        ?Throwable $previous = null,
+        int $code = 0
+    ) {
+        $message = sprintf('Detected %s errors: "%s"', count($errors), implode('", "', $errors));
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * @return array<string,string>
+     */
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+}
