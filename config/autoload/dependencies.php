94fdc6e61c1535b42433bd4128d011024844efa6
William Correa <wilcorrea@gmail.com>
2025-03-06 19:25:36 -0300

    fix: configure dependency factory

--- config/autoload/dependencies.php
+++ config/autoload/dependencies.php
@@ -2,31 +2,27 @@
 
 declare(strict_types=1);
 
-use Hyperf\Contract\ConfigInterface;
-use Psr\Container\ContainerInterface;
+use Psr\Container\ContainerInterface as Container;
 use Psr\Log\LoggerInterface;
 use Serendipity\Example\Game\Domain\Repository\GameCommandRepository;
 use Serendipity\Example\Game\Domain\Repository\GameQueryRepository;
 use Serendipity\Example\Game\Infrastructure\Repository\SleekDB\SleekDBGameCommandRepository;
 use Serendipity\Example\Game\Infrastructure\Repository\SleekDB\SleekDBGameQueryRepository;
+use Serendipity\Hyperf\Database\Document\HyperfSleekDBFactory;
 use Serendipity\Hyperf\Database\Relational\HyperfConnectionFactory;
 use Serendipity\Hyperf\Logging\EnvironmentLoggerFactory;
 use Serendipity\Infrastructure\Database\Document\SleekDBFactory;
 use Serendipity\Infrastructure\Database\Relational\ConnectionFactory;
 
 use function Hyperf\Support\env;
-use function Serendipity\Type\Cast\toArray;
 use function Serendipity\Type\Cast\toString;
 
 return [
-    LoggerInterface::class => fn (ContainerInterface $container) => $container
+    LoggerInterface::class => fn (Container $container) => $container
         ->get(EnvironmentLoggerFactory::class)
         ->make(toString(env('APP_ENV', 'dev'))),
-    SleekDBFactory::class => function (ContainerInterface $container) {
-        $config = $container->get(ConfigInterface::class);
-        $options = toArray($config->get('databases.sleek'));
-        return new SleekDBFactory($options);
-    },
+
+    SleekDBFactory::class => fn (Container $container) => (new HyperfSleekDBFactory())->make($container),
     ConnectionFactory::class => HyperfConnectionFactory::class,
 
     GameCommandRepository::class => SleekDBGameCommandRepository::class,
--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -4,20 +4,12 @@ declare(strict_types=1);
 
 namespace Serendipity;
 
-use Serendipity\Hyperf\Database\Document\HyperfSleekDBFactory;
-use Serendipity\Hyperf\Database\Relational\HyperfConnectionFactory;
-use Serendipity\Infrastructure\Database\Document\SleekDBFactory;
-use Serendipity\Infrastructure\Database\Relational\ConnectionFactory;
-
 class ConfigProvider
 {
     public function __invoke(): array
     {
         return [
-            'dependencies' => [
-                SleekDBFactory::class => HyperfSleekDBFactory::class,
-                ConnectionFactory::class => HyperfConnectionFactory::class,
-            ],
+            'dependencies' => [],
             'annotations' => [
                 'scan' => [
                     'paths' => [
