06efca3097a3a4bd0baa4cabd9a972d048e10063
William Correa <wilcorrea@gmail.com>
2025-03-06 18:34:18 -0300

    feat: review faker structure

---
+++ tests/Hyperf/Database/Document/HyperfSleekDBFactoryTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Serendipity\Test\Hyperf\Database\Document;
+
+use Hyperf\Contract\ConfigInterface;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use Serendipity\Hyperf\Database\Document\HyperfSleekDBFactory;
+use Serendipity\Hyperf\Testing\Extension\MakeExtension;
+
+class HyperfSleekDBFactoryTest extends TestCase
+{
+    use MakeExtension;
+
+    public function testShouldCreateSleekDB(): void
+    {
+        $options = $this->make(ConfigInterface::class)->get('databases.sleek');
+
+        $config = $this->createMock(ConfigInterface::class);
+        $config->expects($this->once())
+            ->method('get')
+            ->with('databases.sleek')
+            ->willReturn($options);
+
+        $container = $this->createMock(ContainerInterface::class);
+        $container->expects($this->once())
+            ->method('get')
+            ->with(ConfigInterface::class)
+            ->willReturn($config);
+
+        $factory = new HyperfSleekDBFactory();
+        $factory->make($container);
+    }
+}
