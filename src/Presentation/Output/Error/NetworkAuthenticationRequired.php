b76ea9bb804f50e02274aa546feeae03cbe03f54
William Correa <wilcorrea@gmail.com>
2025-03-09 17:15:12 -0300

    fix: review php cs lint

--- src/Presentation/Output/Error/NetworkAuthenticationRequired.php
+++ src/Presentation/Output/Error/NetworkAuthenticationRequired.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace Serendipity\Presentation\Output\Error;
 
 /**
- * The client must be authenticated to access the network. The error should include a link where the user can submit credentials.
+ * The client must be authenticated to access the network. The error should include a link where the user can submit
+ * credentials.
  */
 final class NetworkAuthenticationRequired extends Error
 {
